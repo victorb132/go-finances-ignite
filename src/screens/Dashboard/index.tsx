fbad708c21aaaa092b4ae15a6aeb67fe6baf8dc4
victorb132 <victor96novais@gmail.com>
2021-05-24 17:13:04 -0300

    Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-import { Container, Title } from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles'
 
 export function Dashboard() {
-  return(
+  return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/32309553?v=4' }} />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Victor</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
     </Container>
   )
 }
